1a891106406a331e21a95f70532072456648bd60
abhay-dharmale <abhaydharmale9@gmail.com>
2024-04-09 23:18:40 +0530

    change in landing page

--- script.js
+++ script.js
@@ -2,68 +2,51 @@
     const locomotiveScroll = new LocomotiveScroll();
 })();
 
-const navBarAnime = () =>{
-    gsap.to("nav", {
-        y:"-100%",
-        opacity: 0,
-        scrollTrigger: {
-            trigger: "nav",
-            start: "top -10%",
-            end: "top -40%",
-            scrub: 1
-        }
-    });
-};
-
-const menuBtn = () =>{
-    const menuIcon = document.querySelector(".menu-icon");
-    const closeIcon = document.querySelector(".close-icon");
-    const menu = document.querySelector(".menu");
-
-    menuIcon.addEventListener("click", () =>{
-        gsap.to(menu, {
-            right: 0,
-            ease: "power4.inOut",
-        });
-    });
-
-    closeIcon.addEventListener("click", () =>{
-        gsap.to(menu, {
-            right: "-100%",
-            ease: "power4.inOut",
-        });
-    });
-};
-
-const page1Swiper = () =>{
-    const progressCircle = document.querySelector(".autoplay-progress svg");
-    const progressContent = document.querySelector(".autoplay-progress span");
-
-    var swiper = new Swiper(".mySwiper", {
-        speed: 600,
-        parallax: true,
-        loop:true,
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
-        navigation: {
-          nextEl: ".swiper-button-next",
-          prevEl: ".swiper-button-prev",
-        },
-        autoplay: {
-            delay: 4000,
-            disableOnInteraction: false
-          },
-        on: {
-            autoplayTimeLeft(s, time, progress) {
-              progressCircle.style.setProperty("--progress", 1 - progress);
-              progressContent.textContent = `${Math.ceil(time / 1000)}s`;
-            }
+const menuSwiper = () =>{
+  let menuButton = document.querySelector('.menu-button');
+    let openMenu = function () {
+      swiper.slidePrev();
+    };
+    let swiper = new Swiper('.swiper1', {
+      slidesPerView: 'auto',
+      initialSlide: 1,
+      resistanceRatio: 0,
+      slideToClickedSlide: true,
+      on: {
+        slideChangeTransitionStart: function () {
+          let slider = this;
+          if (slider.activeIndex === 0) {
+            menuButton.classList.add('cross');
+            // required because of slideToClickedSlide
+            menuButton.removeEventListener('click', openMenu, true);
+          } else {
+            menuButton.classList.remove('cross');
           }
-      });
-};
+        },
+        slideChangeTransitionEnd: function () {
+          let slider = this;
+          if (slider.activeIndex === 1) {
+            menuButton.addEventListener('click', openMenu, true);
+          }
+        },
+      },
+    });
+}
 
-navBarAnime();
-menuBtn();
-page1Swiper();
+const landingPageSwiper = () =>{
+  let swiper = new Swiper(".landing-page-content .mySwiper", {
+    loop: true,
+    // slidesPerView: 'auto',
+    pagination: {
+      el: ".swiper-pagination",
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-btn-next",
+      prevEl: ".swiper-btn-prev",
+    },
+  });
+}
+
+menuSwiper();
+landingPageSwiper();
